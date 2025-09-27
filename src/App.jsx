9f84bf3c5ed3329bf76a351ed04cf878e87eba10
Andrey Lo <loandrey7@gmail.com>
2025-09-27 18:06:54 +0300

    body

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import { Home, Pages, Services, Project, Blog, Contact } from "./components";
 
 const App = () => {
 	return (
-		<div className="wrapper">
-			<Header />
+		<body>
+			<div className="wrapper">
+				<Header />
 
-			<main className="main">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/Pages" element={<Pages />} />
-					<Route path="/Services" element={<Services />} />
-					<Route path="/Project" element={<Project />} />
-					<Route path="/Blog" element={<Blog />} />
-					<Route path="/Contact" element={<Contact />} />
-				</Routes>
-			</main>
+				<main className="main">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/Pages" element={<Pages />} />
+						<Route path="/Services" element={<Services />} />
+						<Route path="/Project" element={<Project />} />
+						<Route path="/Blog" element={<Blog />} />
+						<Route path="/Contact" element={<Contact />} />
+					</Routes>
+				</main>
 
-			<Footer />
-		</div>
+				<Footer />
+			</div>
+		</body>
 	);
 };
 
