d5ccff94814754d3f2f93ff513570a6abdc6965c
Andrey Lo <loandrey7@gmail.com>
2025-09-28 23:00:27 +0300

    mobile-nav v4.7

--- src/hooks/useMobileNav.js
+++ src/hooks/useMobileNav.js
@@ -1,22 +1,3 @@
-// // src/hooks/useMobileNav.js
-// import { useState } from 'react';
-
-// const useMobileNav = () => {
-// 	const [isOpen, setIsOpen] = useState(false);
-
-// 	const toggleNav = () => {
-// 		setIsOpen(prevState => !prevState);
-// 		document.body.classList.toggle('no-scroll', !isOpen);
-// 	};
-
-// 	return {
-// 		isOpen,
-// 		toggleNav,
-// 	};
-// };
-
-// export default useMobileNav;    
-
 // src/hooks/useMobileNav.js
 import { useState, useEffect } from 'react';
 
@@ -71,4 +52,3 @@ const useMobileNav = () => {
 };
 
 export default useMobileNav;
-
